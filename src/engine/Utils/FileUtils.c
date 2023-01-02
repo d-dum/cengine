e13352480e289e6a3ce717c20e7cf068557bc6a5
d-dum <levdenisov666@gmail.com>
2023-01-02 23:10:47 +0100

    read bmp

--- src/engine/Utils/FileUtils.c
+++ src/engine/Utils/FileUtils.c
@@ -7,6 +7,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+
 // TODO: error handling
 
 char* readFile(char* path){
@@ -30,4 +32,46 @@ char* readFile(char* path){
     ret[fSize] = 0;
 
     return ret;
+}
+
+
+BMPImage* readBMP(char* path){
+    FILE* file = fopen(path, "rb");
+    if(!file){
+        fprintf(stderr, "BMP file %s doesn't exist\n", path);
+        return NULL;
+    }
+
+    unsigned char header[54];
+    if(fread(header, 1, 54, file) != 54){
+        fprintf(stderr, "Not a correct BMP file\n");
+        return NULL;
+    }
+
+    if(header[0] != 'B' || header[1] != 'M'){
+        fprintf(stderr, "Not a correct BMP file\n");
+        return NULL;
+    }
+
+    BMPImage* image = (BMPImage*) malloc(sizeof(BMPImage));
+
+    image->dataPos = *(int*)&(header[0x0A]);
+    image->imageSize = *(int*)&(header[0x22]);
+    image->width = *(int*)&(header[0x12]);
+    image->height = *(int*)&(header[0x16]);
+
+    if(image->imageSize == 0) image->imageSize = image->width * image->height * 3;
+    if(image->dataPos == 0) image->dataPos = 54;
+
+    image->data = (unsigned char*) malloc(sizeof(char)*image->imageSize);
+
+    fread(image->data, 1, image->imageSize, file);
+    fclose(file);
+
+    return image;
+}
+
+void freeBMP(BMPImage* image){
+    free(image->data);
+    free(image);
 }
--- src/engine/Utils/FileUtils.h
+++ src/engine/Utils/FileUtils.h
@@ -5,6 +5,16 @@
 #ifndef ENGINE_FILEUTILS_H
 #define ENGINE_FILEUTILS_H
 
+typedef struct{
+    unsigned int dataPos;
+    unsigned int width;
+    unsigned int height;
+    unsigned int imageSize;
+    unsigned char* data;
+} BMPImage;
+
 char* readFile(char* path);
+BMPImage* readBMP(char* path);
+void freeBMP(BMPImage* image);
 
 #endif //ENGINE_FILEUTILS_H
