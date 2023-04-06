a085380d18112034182074a357ca5d893ae242ba
d-dum <levdenisov666@gmail.com>
2023-04-06 14:13:54 +0200

    png loading (will test later)

--- src/engine/Utils/FileUtils.c
+++ src/engine/Utils/FileUtils.c
@@ -7,9 +7,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
-
-// TODO: error handling
+#include <png.h>
 
 char* readFile(char* path){
     FILE* f = fopen(path, "r");
@@ -72,6 +70,91 @@ BMPImage* readBMP(char* path){
 }
 
 void freeBMP(BMPImage* image){
+    free(image->data);
+    free(image);
+}
+
+PNGImage* readPNG(char* path){
+    FILE* file = fopen(path, "rb");
+    if(!file){
+        printf("Error: failed to open file %s\n", path);
+        return NULL;
+    }
+
+    png_structp pngPtr = png_create_read_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+    if(!pngPtr){
+        printf("Error: png_create_read_struct failed\n");
+        fclose(file);
+        return NULL;
+    }
+
+    png_infop infoPtr = png_create_info_struct(pngPtr);
+    if (!infoPtr) {
+        printf("Error: png_create_info_struct failed\n");
+        png_destroy_read_struct(&pngPtr, NULL, NULL);
+        fclose(file);
+        return NULL;
+    }
+
+    if (setjmp(png_jmpbuf(pngPtr))) {
+        printf("Error: setjmp failed\n");
+        png_destroy_read_struct(&pngPtr, &infoPtr, NULL);
+        fclose(file);
+        return NULL;
+    }
+
+    png_init_io(pngPtr, file);
+    png_read_info(pngPtr, infoPtr);
+
+    png_byte colorType = png_get_color_type(pngPtr, infoPtr);
+    png_byte bitDepth = png_get_bit_depth(pngPtr, infoPtr);
+
+    if (bitDepth == 16) {
+        png_set_strip_16(pngPtr);
+    }
+
+    if (colorType == PNG_COLOR_TYPE_PALETTE) {
+        png_set_palette_to_rgb(pngPtr);
+    }
+
+    if (colorType == PNG_COLOR_TYPE_GRAY && bitDepth < 8) {
+        png_set_expand_gray_1_2_4_to_8(pngPtr);
+    }
+
+    if (png_get_valid(pngPtr, infoPtr, PNG_INFO_tRNS)) {
+        png_set_tRNS_to_alpha(pngPtr);
+    } else {
+        png_set_filler(pngPtr, 0xFF, PNG_FILLER_AFTER);
+    }
+
+    png_read_update_info(pngPtr, infoPtr);
+
+    png_uint_32 width = png_get_image_width(pngPtr, infoPtr);
+    png_uint_32 height = png_get_image_height(pngPtr, infoPtr);
+    png_byte channels = png_get_channels(pngPtr, infoPtr);
+
+    PNGImage* image = malloc(sizeof(PNGImage));
+    image->width = width;
+    image->height = height;
+    image->imageSize = width * height * channels;
+
+    image->data = (unsigned char*)malloc(image->imageSize);
+
+    png_bytep* rowPointers = malloc(sizeof(png_bytep) * height);
+
+    for (unsigned int i = 0; i < height; ++i) {
+        rowPointers[i] = image->data + i * width * channels;
+    }
+
+    png_read_image(pngPtr, rowPointers);
+
+    png_destroy_read_struct(&pngPtr, &infoPtr, NULL);
+    fclose(file);
+
+    return image;
+}
+
+void freePNG(PNGImage* image){
     free(image->data);
     free(image);
 }
--- src/engine/Utils/FileUtils.h
+++ src/engine/Utils/FileUtils.h
@@ -13,8 +13,17 @@ typedef struct{
     unsigned char* data;
 } BMPImage;
 
+typedef struct{
+    unsigned int width;
+    unsigned int height;
+    unsigned int imageSize;
+    unsigned char* data;
+} PNGImage;
+
 char* readFile(char* path);
 BMPImage* readBMP(char* path);
 void freeBMP(BMPImage* image);
+PNGImage* readPNG(char* path);
+void freePNG(PNGImage* image);
 
 #endif //ENGINE_FILEUTILS_H
